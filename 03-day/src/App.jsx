00470e67a84af7b508d73ed6367f70e7aaa73c7c
NehalAhmedQureshi <m.nehalq125@gmail.com>
2024-12-04 19:02:20 +0500

    update react project

--- 03-day/src/App.jsx
+++ 03-day/src/App.jsx
@@ -2,33 +2,48 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react'
+function Button() {
+  var [count, setCount] = useState(0)
+  let [autoUpd, setAutoUpd] = useState(false)
+  useEffect(() => {
+    let timeOutID;
+
+    if (autoUpd) {
+      timeOutID = setTimeout(() => {
+        setCount((prevCount) => prevCount + 1);
+      }, 3000);
+    }
+
+    return () => {
+      // Cleanup the timeout when the component unmounts or autoUpd changes
+      clearTimeout(timeOutID);
+    };})
+  return (
+    <div className="main">
+      <h2 className="show">
+        Your Results : {count}
+      </h2>
+      <button className="addbtn" onClick={() => { setCount(count + 1) }}>
+        Increase
+      </button>
+      <button className="dlbtn" onClick={() => {
+        setCount(count - 1)
+      }}>
+        Decrease
+      </button>
+      <button onClick={() => { setAutoUpd(!autoUpd) }}>Auto Update {autoUpd ? 'On' : 'Off'}</button>
+    </div>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="main">
+      <Button />
+    </div>
   )
 }
 
